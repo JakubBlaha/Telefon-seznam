2f6a6ca5b777f00409c8902e20fbd6c481efdeab
JakubBlaha <jakub.blaha@gmail.com>
2020-04-20 22:47:26 +0200

    Fixed data.c

--- data.c
+++ data.c
@@ -1,37 +1,5 @@
-#ifndef DATA_H_INCLUDED
-#define DATA_H_INCLUDED
+#include "data.h"
 
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-
-typedef unsigned int uint;
-
-#define MAXLEN 21
-
-/** \brief Jeden prvek v telefonním seznamu. */
-typedef struct _zaznam
-{
-    char jmeno[MAXLEN]; /**< Jméno osoby */
-    uint cislo;         /**< Telefonní číslo */
-    bool platny;        /**< True znamená, že je prvek platný, false, že je smazaný. */
-} Tzaznam;
-
-/** \brief Struktura reprezentující telefonní seznam, tedy pole záznamů. */
-typedef struct
-{
-    Tzaznam zaznam[MAXSEZNAM]; /**< Pole záznamů. */
-    uint delkaSerazene;        /**< Délka seřazené části pole. Zároveň index začátku oblasti přeplnění. */
-    uint pocet;                /**< Počet využitých prvků pole (včetně smazaných). */
-    uint pocetSmazanych;       /**< Aktuální počet smazaných prvků. */
-    long nalezeny;             /**< Index posledního nalezeného prvku. */
-} Tseznam;
-
-/** \brief Vyrobí prázdný seznam.
- *
- * \param void
- * \return Tseznam Vrací prázdný seznam.
- */
 Tseznam prazdnySeznam(void)
 {
     // Vrátíme pointer na strukturu seznam typu Tseznam
@@ -39,10 +7,6 @@ Tseznam prazdnySeznam(void)
     return seznam;
 }
 
-/** \brief Přemaže existující seznam.
- *
- * \param seznam Tseznam* Existující seznam předávaný odkazem.
- */
 void resetujSeznam(Tseznam *seznam)
 {
     // Nemusíme nic přemazávat, stačí nastavit pocet záznamů na 0
@@ -50,22 +14,13 @@ void resetujSeznam(Tseznam *seznam)
 
     for (int i = 0; i < MAXSEZNAM; i++)
     {
-        seznam -> zaznam[i].platny = false;
+        seznam->zaznam[i].platny = false;
     }
 
     // Resetujeme i hledání
     resetujHledani(seznam);
 }
 
-/** \brief Vloží nový záznam do telefonního seznamu. Nekontroluje, jestli už takový záznam existuje.
- *
- * Vložení se nemusí povést, když v seznamu už není dostatek místa.
- *
- * \param seznam Tseznam* Seznam předávaný odkazem.
- * \param jmeno char*  Jméno osoby (bude se kopírovat do seznamu).
- * \param cislo uint   Telefonní číslo.
- * \return bool Vrací true, když se vložení povede, jinak vrací false.
- */
 bool vlozZaznam(Tseznam *seznam, char jmeno[], uint cislo)
 {
     // Pokud je už seznam plný, vrátíme false a skončíme
@@ -99,26 +54,6 @@ bool vlozZaznam(Tseznam *seznam, char jmeno[], uint cislo)
     return true;
 }
 
-/** \brief Najde další telefonní číslo pro zadané jméno.
- *
- * Každé jméno může mít uloženo několik čísel.
- * Hledání začíná od začátku, pokud ještě žádné hledání neproběhlo, nebo byla
- * před hledáním zavolána funkce resetujHledani(). V opačném případě začíná od
- * posledního výsledku.
- *
- * Pozor! Pokud se další hledání odehraje pro jiné jméno, nemusí být výsledek
- * hledání zcela správný.
- *
- * Poznámka: Pokud se implementace seznamu změní, může mít tato funkce pořád
- * stejné rozhraní.
- *
- * \param seznam Tseznam* Seznam předávaný odkazem.
- * \param jmeno char*  Jméno, podle kterého se bude v seznamu hledat.
- * \param cislo uint*  Nalezené telefonní číslo. Proměnná předávaná odkazem.
- *                     Výsledek je platný, když funkce vrátí true.
- * \return bool Vrací true, když funkce našla záznam, jinak vrací false a hodnota
- *              v parametru cislo nemá smysl.
- */
 bool najdiDalsiCislo(Tseznam *seznam, char jmeno[], uint *cislo)
 {
     Tzaznam zaznam;
@@ -138,27 +73,11 @@ bool najdiDalsiCislo(Tseznam *seznam, char jmeno[], uint *cislo)
     return false;
 }
 
-/** \brief Resetuje hledání, tj. nastaví index posledního nalezeného prvku na -1 (tj. nenalezeno).
- *
- * Index začátku dalšího hledání je položka nalezeny v seznamu.
- *
- * \param seznam Tseznam* Seznam předávaný odkazem.
- */
 void resetujHledani(Tseznam *seznam)
 {
     seznam->nalezeny = -1;
 }
 
-/** \brief Odebere poslední nalezený záznam ze seznamu.
- *
- * Index posledního nalezeného záznamu je v položce nalezeny v seznamu. Pokud je
- * tato hodnota neplatná (nastavená na -1), nic se neodebere a funkce vrátí false.
- *
- * Volá funkci resetujHledani().
- *
- * \param seznam Tseznam* Seznam předávaný odkazem.
- * \return bool Vrací true, když dojde k odebrání záznamu, jinak vrací false.
- */
 bool odeberPosledniNalezenyZaznam(Tseznam *seznam)
 {
     // Vrátíme false, pokud se nic předtím nenalezlo
@@ -200,12 +119,6 @@ void sort(Tseznam *seznam)
     }
 }
 
-/** \brief Fyzicky provede nahromaděné změny (mazání prvků, seřazení a sloučení s oblastí přeplnění).
- *
- * Volá funkci resetujHledani().
- *
- * \param seznam Tseznam* Seznam předávaný odkazem.
- */
 void aktualizujSeznam(Tseznam *seznam)
 {
     // Zresetujeme hledání
@@ -248,5 +161,3 @@ void aktualizujSeznam(Tseznam *seznam)
     puts("Sortim...");
     sort(seznam);
 }
-
-#endif // DATA_H_INCLUDED
